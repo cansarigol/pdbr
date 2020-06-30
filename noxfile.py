7ed198e29ec66b19b3cbc1be9ccacdacf03058fd
Can Sarigol <cansarigol@derinbilgi.com.tr>
2020-06-30 19:28:26 +0200

    Added reuse_venv for nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 @nox.session
-def lint(session):
+def lint(session, reuse_venv=True):
     session.install('autoflake', 'isort', 'black')
     session.run('autoflake', "--in-place", "--recursive", 'pdbr', 'tests')
     session.run('black', 'pdbr', 'tests')
@@ -9,7 +9,7 @@ def lint(session):
 
 
 @nox.session
-def check(session):
+def check(session, reuse_venv=True):
     session.install('flake8', 'isort', 'black')
     session.run('flake8', 'pdbr', 'tests')
     session.run("black", "--check", "--diff", 'pdbr', 'tests')
@@ -17,6 +17,6 @@ def check(session):
 
 
 @nox.session(python=["3.6", "3.7", "3.8"])
-def test(session):
+def test(session, reuse_venv=True):
     session.install('pytest', 'rich')
     session.run('pytest')
