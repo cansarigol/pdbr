d0a63add188f432a7a0b6e7d99e8b42f4bfbf0c6
Can Sarigol <ertugrulsarigol@gmail.com>
2021-03-06 01:25:38 +0100

    lint

--- pdbr/pdbr_shell.py
+++ pdbr/pdbr_shell.py
@@ -11,7 +11,8 @@ def cli():
         from traitlets import Type
     except ModuleNotFoundError as error:
         raise type(error)(
-            "In order to use pdbr shell, install IPython with pdbr[ipython]") from error
+            "In order to use pdbr shell, install IPython with pdbr[ipython]"
+        ) from error
 
     class PdbrTerminalInteractiveShell(TerminalInteractiveShell):
         @property
@@ -20,9 +21,12 @@ def cli():
 
     class PdbrTerminalIPythonApp(TerminalIPythonApp):
         interactive_shell_class = Type(
-            klass=object,   # use default_value otherwise which only allow subclasses.
+            klass=object,  # use default_value otherwise which only allow subclasses.
             default_value=PdbrTerminalInteractiveShell,
-            help="Class to use to instantiate the TerminalInteractiveShell object. Useful for custom Frontends"
+            help=(
+                "Class to use to instantiate the TerminalInteractiveShell object. "
+                "Useful for custom Frontends"
+            ),
         ).tag(config=True)
 
     app = PdbrTerminalIPythonApp.instance()
